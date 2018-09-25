3d405339666650a98900d9c8cecc2c59d9b103ba
John Treasure <jhntreasure@yahoo.co.uk>
2018-09-25 20:30:48 +0100

    Utilising Arrays for the board
    Improved the board by using array methods rather than just manually creating the board

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,13 +1,18 @@
-const blankLine = ' |   | '
-const guessLine = '1|   | '
-const bombLine = ' | B | '
-console.log('This is what an empty board would look like:')
+const printBoard = board => {
+  console.log('Current board: ');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '))
+}
 
-console.log(blankLine)
-console.log(blankLine)
-console.log(blankLine)
+const board = [
+  [' ',' ',' '],
+  [' ',' ',' '],
+  [' ',' ',' ']
+];
 
-console.log('This is what a board with a guess and a bomb on it would look like:')
-console.log(guessLine)
-console.log(bombLine)
-console.log(blankLine)
+printBoard(board)
+
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board)
